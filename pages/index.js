9751d9d82f7115c2553648a8b7bf093cbe7bec29
KevinOlaf <34963633+KevinOlaf@users.noreply.github.com>
2024-02-02 09:09:12 +0700

    modified:   pages/index.js 	modified:   styles/globals.css

--- pages/index.js
+++ pages/index.js
@@ -15,34 +15,18 @@ export default function Home(props) {
       </Head>
       <div className="home">
         <h1 className="home__title">
-          Welcome to <br />
+          ศูนย์ดำรงธรรม <br />
           <a
             className="home__title__link"
             href="https://developers.line.biz/en/docs/liff/overview/"
           >
-            LIFF Starter!
+            จังหวัดฉะเชิงเทรา
           </a>
         </h1>
-        <div className="home__badges">
-          <span className="home__badges__badge badge--primary">
-            LIFF Starter
-          </span>
-          <span className="home__badges__badge badge--secondary">nextjs</span>
-          <span className="home__badges__badge badge--primary">
-            {packageJson.version}
-          </span>
-          <a
-            href="https://github.com/line/line-liff-v2-starter"
-            target="_blank"
-            rel="noreferrer"
-            className="home__badges__badge badge--secondary"
-          >
-            GitHub
-          </a>
-        </div>
+      
         <div className="home__buttons">
           <a
-            href="https://developers.line.biz/en/docs/liff/developing-liff-apps/"
+            href="https://www.chachoengsao.go.th/"
             target="_blank"
             rel="noreferrer"
             className="home__buttons__button button--primary"
